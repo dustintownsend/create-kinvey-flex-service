a1ea46d2875cec98d40dd1157b1652c5d9ed89d9
Dustin Townsend <dustin.townsend@gmail.com>
2019-01-30 22:05:14 -0500

    update template and some deps

--- packages/kinvey-flex-scripts/template/src/index.js
+++ packages/kinvey-flex-scripts/template/src/index.js
@@ -1,1 +1,11 @@
-console.log('Just a placeholder');
+import * as sdk from 'kinvey-flex-sdk';
+
+sdk.service((err, flex) => {
+  const {
+    functions
+  } = flex;
+
+  functions.register('firstFunction', function (context, complete, /* modules */) {
+    return complete('success').ok().next();
+  });
+});
